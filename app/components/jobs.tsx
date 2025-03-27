5730e90136ad817b11c1a0b2729627a727321c3a
PikachuUsedSurf <nanaosafokissi@gmail.com>
2025-03-27 15:23:30 +0300

    added drawer

--- app/components/jobs.tsx
+++ app/components/jobs.tsx
@@ -1,9 +1,9 @@
+"use client"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import type {JobData} from "@/../../app/page"
-
-
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
 
 
 export default function Job({ JobData }: { JobData: JobData }) {
@@ -20,10 +20,26 @@ export default function Job({ JobData }: { JobData: JobData }) {
               <p>{JobData.salary ? JobData.salary : "Salary not disclosed"}</p>
             </CardContent>
             <CardFooter>
-              <Button>Apply here!</Button>
+              <Drawer>
+                <DrawerTrigger>
+                  <Button>apply here</Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                  <div className="mx-auto w-full max-w-sm">
+                    <DrawerHeader>
+                      <DrawerTitle>
+                        Apply for {JobData.title} at {JobData.company}
+                      </DrawerTitle>
+                      <DrawerDescription>
+                        Please fill out the application form below.
+                      </DrawerDescription>
+                    </DrawerHeader>
+                  </div>
+                </DrawerContent>
+              </Drawer>
             </CardFooter>
           </Card>
-          </div>
+        </div>
       )
     }
 
